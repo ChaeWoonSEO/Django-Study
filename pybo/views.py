925a6b0147f65f76abee96b402835c25548faea7
lot8229 <lot8229@kakao.com>
2021-12-09 17:22:48 +0900

    Edit Config & Views for Templetes

--- pybo/views.py
+++ pybo/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
+from .models import Question
 def index(requset):
-    return HttpResponse("Welcome to pybo")
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list':question_list}
+    return render(requset,'pybo/question_list.html',context)
