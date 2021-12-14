637b48dbb4d84281aca838d499c38ab295aeb876
lot8229 <lot8229@kakao.com>
2021-12-14 10:56:23 +0900

    조회 템플릿 추가 및 url 생성

--- pybo/views.py
+++ pybo/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Question
+
 def index(requset):
-    """
-    pybo 목록 출력
-    """
     question_list = Question.objects.order_by('-create_date')
     context = {'question_list':question_list}
     return render(requset,'pybo/question_list.html',context)
+
+def detail(request,question_id):
+    question = Question.objects.get(id=question_id)
+    context = {'question':question}
+    return render(request,'pybo/question_detail.html',context)
